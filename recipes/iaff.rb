732f7118fb3b1a6fb29e5c44f54087cf96f124a9
Tyler Garner <garnertb@gmail.com>
2015-04-28 07:31:17 -0400

    Add the firestation view.

--- recipes/iaff.rb
+++ recipes/iaff.rb
@@ -10,6 +10,51 @@ execute "sync_db" do
   command "#{node['rogue']['geonode']['location']}bin/python #{node['rogue']['rogue_geonode']['location']}/manage.py syncdb --no-initial-data --all"
 end
 
+execute 'create_firestation_view' do
+  user 'postgres'
+  command <<-EOF
+
+DROP VIEW IF EXISTS firestations;
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Name: firestations; Type: VIEW; Schema: public; Owner: -
+--
+
+CREATE VIEW firestations AS
+ SELECT
+    a.name,
+    a.address,
+    a.city,
+    a.state,
+    a.zipcode,
+    a.geom,
+    '#{node['rogue']['networking']['application']['fqdn']}/jurisdictions/fire-stations/' || a.id AS "URL",
+    COALESCE(sum(d.firefighter), 0) AS "Total Firefighters",
+    COALESCE(sum(d.firefighter_emt), 0) AS "Total Firefighter/EMTS",
+    COALESCE(sum(d.firefighter_paramedic), 0) AS "Total Firefighter/Paramedics",
+    COALESCE(sum(d.ems_emt), 0) AS "Total EMS only EMTs",
+    COALESCE(sum(d.ems_paramedic), 0) AS "Total EMS only Paramedics",
+    COALESCE(sum(d.officer), 0) AS "Total Officers",
+    COALESCE(sum(d.officer_paramedic), 0) AS "Total Officer/Paramedics",
+    COALESCE(sum(d.ems_supervisor), 0) AS "Total EMS Supervisors",
+    COALESCE(sum(d.chief_officer), 0) AS "Total Chief Officers"
+   FROM (firestation_usgsstructuredata a
+     JOIN firestation_firestation b ON (b.usgsstructuredata_ptr_id = a.id)
+     LEFT JOIN firestation_staffing d ON (b.usgsstructuredata_ptr_id = d.firestation_id))
+  GROUP BY a.id;
+  EOF
+  action :run
+end
+
 
 file "/etc/cron.d/geoshape_update_data" do
   content "*/30 * * * * rogue /var/lib/geonode/bin/python /var/lib/geonode/rogue_geonode/manage.py update_data > /dev/null\n"
